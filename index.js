0c2edc5493b7a6ef3f7d710ca9692a0e7adc6e25
Robert Daub <rjd2066@gmail.com>
2020-04-03 04:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidaysTo(name = Jessie) {
 }
 
 function happyCustomHolidayTo(holiday = Independence, name = Jessie) {
-  return `Happy ${holiday}s, ${name}`;
+  return `Happy ${holiday}s, ${name}!`;
 }
