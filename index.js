c6a207c6260b2b68e983fa7042b215207057d4ff
Robert Daub <rjd2066@gmail.com>
2020-04-03 04:39:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function happyHolidaysTo(name = Jessie) {
 function happyCustomHolidayTo(holiday = Independence, name = Jessie) {
   return `Happy ${holiday}, ${name}!`;
 }
+
+function holidayCountdown(holiday = Independence, days = 45) {
+  return `It's ${days} days until ${holiday}!`
+}
